6d48d25d540da98487c39b86a64085d9a84aa54e
Yash Mehta <yashmehta0395@gmail.com>
2023-11-25 12:24:51 +0530

    FormField setup done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
@@ -8,7 +9,7 @@ const App = () => (
   <BrowserRouter>
     <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
       <Link to="/">
-        <img src={logo} alt="logo" className="w-10 h-28 object-contain" />
+        <img src={logo} alt="logo" className="object-contain w-[28px]" />
       </Link>
 
       <Link to="/create-post" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">Create</Link>
