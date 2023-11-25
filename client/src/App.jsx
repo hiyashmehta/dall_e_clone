1cdacaef36bed4946f900165da26d1b115899542
Yash Mehta <yashmehta0395@gmail.com>
2023-11-25 19:24:27 +0530

    App is ready and all errors got resolved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ const App = () => (
   <BrowserRouter>
     <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
       <Link to="/">
-        <img src={logo} alt="logo" className="object-contain w-[28px]" />
+        <img src={logo} alt="logo" className="object-contain w-28" />
       </Link>
 
       <Link to="/create-post" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">Create</Link>
